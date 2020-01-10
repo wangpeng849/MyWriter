306f0fe3b43f9e22cdaac39107aad2f75207692d
wangpeng849 <849183239@qq.com>
2020-01-10 17:32:50 +0800

    文件操作工具類

--- src/main/java/com/wangp/myaop/serializationobject/SerializationObject.java
+++ src/main/java/com/wangp/myaop/serializationobject/SerializationObject.java
@@ -9,7 +9,9 @@ import lombok.Data;
 import org.nustaq.serialization.FSTConfiguration;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
---
+++ src/main/java/com/wangp/myaop/util/FileUtil.java
@@ -0,0 +1,73 @@
+package com.wangp.myaop.util;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author wangp
+ * @Date 2020/1/10
+ * @Version 1.0
+ */
+public class FileUtil {
+
+    /**
+     * 刪除除關鍵詞外的文件
+     * @param file
+     * @param names
+     */
+    public static void deleteFileByNotContainsName(File file, List names) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File file1 : files) {
+                deleteFileByNotContainsName(file1, names);
+            }
+        } else {
+            if (!strListContainStr(names, file.getName())) {
+                System.out.println(file.getName() + "is deleted..");
+                file.delete();
+            }
+        }
+    }
+
+
+    /**
+     * 判斷文件是否存在關鍵詞集合中
+     * @param strList
+     * @param str
+     * @return
+     */
+    public static boolean strListContainStr(List<String> strList, String str) {
+        for (String s : strList) {
+            if (str.contains(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 刪除空白文件夾
+     * @param file
+     */
+    public static void deleteBlankDirectory(File file){
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            if(files.length == 0){
+                file.delete();
+            }else{
+                for (File file1 : files) {
+                    System.out.println(file.getName() + "is deleted..");
+                    deleteBlankDirectory(file1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File("E:\\test");
+//        deleteFileByNotContainsName(file, Arrays.asList("王鵬","王"));
+        deleteBlankDirectory(file);
+    }
+}
