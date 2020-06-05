74a156f3f6cd86ffc1e62d859c2fef46d984e9aa
wangpeng849 <849183239@qq.com>
2020-06-05 13:53:54 +0800

    选择排序补充

--- src/main/java/com/wangp/myaop/sort_algorithm/cmp/SelectionSort.java
+++ src/main/java/com/wangp/myaop/sort_algorithm/cmp/SelectionSort.java
@@ -11,7 +11,10 @@ public class SelectionSort<E  extends Comparable<E>> extends Sort<E> {
         for (int end = array.length - 1; end > 0; end--) {
             int maxIndex = 0;
             for (int begin = 0; begin <= end; begin++) {
-                // = 是为了排序算法的稳定性
+                // = 是为了排序算法的稳定性 仍然不是稳定的排序
+                //如 7 5 10 10 2a 4 2b
+                //   7 5 10 2b 2a 4 10
+                //   ...
                 if (cmp(maxIndex, begin) <= 0) {
                     maxIndex = begin;
                 }
