6a6a06076727d6cc8319c0a623016ebdac17371f
wangpeng <wangpeng@shangminet.net>
2020-08-19 20:08:27 +0800

    小随笔

--- src/main/java/com/wangp/myaop/util/GiveValueTest.java
+++ src/main/java/com/wangp/myaop/util/GiveValueTest.java
@@ -2,6 +2,8 @@ package com.wangp.myaop.util;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -19,15 +21,21 @@ public class GiveValueTest {
     public static void main(String[] args) {
         List<String> list = Lists.newArrayList();
         Map<String, String> map = Maps.newHashMap();
-        String str = "test";
-        processList(list, map, str);
+        processList(list, map);
+        String str = "hello={0},world={1}";
+        List<String> param = Arrays.asList("test", "other");
+        String format = "";
+        for (String s : param) {
+            str = MessageFormat.format(str, "test", "other");
+        }
+        System.out.println(str);
 
         System.out.println(list);
         System.out.println(map);
-        System.out.println(str);
+
     }
 
-    public static void processList(List<String> list, Map<String, String> map, String str) {
+    public static void processList(List<String> list, Map<String, String> map) {
 //        list = Arrays.asList("aaa", "bbb", "ccc");  --> not value
         list.add("eee");
         map.put("aaa", "ccc");
