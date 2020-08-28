6c43d0f66ca227a9f00f3c8d9f686749127f041f
wangpeng <wangpeng@shangminet.net>
2020-08-28 10:59:58 +0800

    ApplicationStartedEvent

---
+++ src/main/java/com/wangp/myaop/xuyu/applicationctx/TestClass3.java
@@ -0,0 +1,15 @@
+package com.wangp.myaop.xuyu.applicationctx;
+
+/**
+ * <pre>
+ * classname TestClass
+ * description
+ * </pre>
+ *
+ * @author wangp
+ * @date 2020/8/26 11:34
+ **/
+@Test
+public class TestClass3 {
+
+}
--- src/main/java/com/wangp/myaop/xuyu/applicationctx/TestHandler.java
+++ src/main/java/com/wangp/myaop/xuyu/applicationctx/TestHandler.java
@@ -1,8 +1,8 @@
 package com.wangp.myaop.xuyu.applicationctx;
 
 import java.util.Map;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,13 +13,16 @@ import org.springframework.stereotype.Component;
  *
  * @author wangp
  * @date 2020/8/26 11:32
+ * <p>
+ * ContextRefreshedEvent 与 ApplicationStartedEvent ？
  **/
 @Component
-public class TestHandler implements ApplicationListener<ContextRefreshedEvent> {
+public class TestHandler implements ApplicationListener<ApplicationStartedEvent> {
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(ApplicationStartedEvent event) {
         Map<String, Object> beansWithAnnotation = event.getApplicationContext().getBeansWithAnnotation(Test.class);
-        System.out.println(beansWithAnnotation);
+        beansWithAnnotation.values()
+                .forEach(e -> System.out.println("-------register-------" + e + "-------------------"));
     }
 }
