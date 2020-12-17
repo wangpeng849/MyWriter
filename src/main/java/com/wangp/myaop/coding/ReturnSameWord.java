bf05f5941f39eb501bdc97961617136c23b4b74a
wangpeng <7886443+wang_ts@user.noreply.gitee.com>
2020-12-17 10:22:27 +0800

    做题

--- src/main/java/com/wangp/myaop/coding/ReturnSameWord.java
+++ src/main/java/com/wangp/myaop/coding/ReturnSameWord.java
@@ -1,27 +1,27 @@
 package com.wangp.myaop.coding;
 
 public class ReturnSameWord {
+
     /**
      * 返回两个字符串公共单词
+     * <p>
+     * 由于单词间有空格 根据空格将字符串切割成字符串数组 然后对数组进行比较
      *
-     * 由于单词间有空格
-     * 根据空格将字符串切割成字符串数组
-     * 然后对数组进行比较
      * @param s
      * @param t
      * @return
      */
-    public static String returnSameWord(String s, String t){
+    public static String returnSameWord(String s, String t) {
         String[] sArr = str2Arr(s);
         String[] tArr = str2Arr(t);
 
-        for (int i = 0; i < sArr.length; i++) {
-            for (int j = 0; j < tArr.length; j++) {
-                if("".equals(tArr[j]) | "".equals(sArr[i])){
+        for (String item : sArr) {
+            for (String value : tArr) {
+                if ("".equals(value) | "".equals(item)) {
                     break;
                 }
-                if(sArr[i].equals(tArr[j])){
-                    return sArr[i];
+                if (item.equals(value)) {
+                    return item;
                 }
             }
         }
@@ -30,10 +30,11 @@ public class ReturnSameWord {
 
     /**
      * 字符串转数组
+     *
      * @param str
      * @return
      */
-    public static String[] str2Arr(String str){
+    public static String[] str2Arr(String str) {
         String[] result = new String[100];
         for (int i = 0; i < result.length; i++) {
             result[i] = "";
@@ -41,11 +42,11 @@ public class ReturnSameWord {
         //数组计数
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if(' '==str.charAt(i)){
-                count ++;
+            if (' ' == str.charAt(i)) {
+                count++;
                 continue;
             }
-            result[count]+=str.charAt(i)+"";
+            result[count] += str.charAt(i) + "";
         }
         return result;
     }
